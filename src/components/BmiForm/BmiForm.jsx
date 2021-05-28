6768bc1b07120aae42453178bea95c2f34d54d62
rumesh <rumesh.m.s16@gmail.com>
2021-05-28 15:33:01 +0530

    added speech recog

--- src/components/BmiForm/BmiForm.jsx
+++ src/components/BmiForm/BmiForm.jsx
@@ -1,15 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import '../App/App.css';
+// import Dictaphone from '../SpeechRec/SpeechRec';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 const initialValues = {
 	weight: '',
 	height: '',
 	date: ''
 }
+let speechRecognitionOn = false;
 
 const BmiForm = ({ change }) => {
 	const [state, setState] = useState(initialValues);
+	const [speechValue, setSpeechValue] = useState('');
+	const { speak,cancel,speaking} = useSpeechSynthesis();
+	const { transcript, resetTranscript } = useSpeechRecognition();
+	const contListen = ()=>{ //continuously listen from mic
+
+		if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+			console.error("Browser not supported. Use Chrome")
+			return null
+		}
+		speechRecognitionOn = ! speechRecognitionOn;
+		if (speechRecognitionOn){
+			setSpeechValue("Speech Recognition started");
+			return SpeechRecognition.startListening({ continuous: true,language: 'en-IN' })
+		}else{
+			setSpeechValue("Speech Recognition stopped");
+			SpeechRecognition.stopListening();
+		}
+
+	}
+
+
+	const HiddenPara = ({text})=>{
+		useEffect(()=>{
+			if (text===""|| text===speechValue){
+				return;
+			}
+
+			if (text.includes("off")|| text.includes("shut")|| text.includes("kill")||text.includes("reset")||text.includes("stop")){
+				SpeechRecognition.abortListening();
+				setSpeechValue("Speech Recognition stopped")
+			}
+			setSpeechValue(text.toString());
+			console.log(speechValue);
+		},[speechValue])
+
+		return (<p>transcript: </p>)
+	}
+
+	const SpeakPara = ({textToSpeak})=>{
+		useEffect(()=>{
+			if (speaking){
+				cancel()
+			}
+			speak({ text: textToSpeak })
+		})
+		return (
+			<p>{textToSpeak}</p>
+		)
+	}
 
 	const handleChange = e => {
 		let { value, name } = e.target;
@@ -70,6 +123,18 @@ const BmiForm = ({ change }) => {
 				>
 					Calculate BMI
 				</button>
+
+				<div>
+					<button onClick={contListen}>Toggle Speech Recognition</button>
+					{/*//todo apply style, move to right upper corner*/}
+					<button onClick={resetTranscript}>Reset</button>
+					{/*//todo this should be hidden*/}
+					<HiddenPara text={transcript}/>
+					{/*//todo this should be hidden*/}
+					<SpeakPara textToSpeak={"Hello"}/>
+
+
+				</div>
 			</div>
 		</>
 	);
